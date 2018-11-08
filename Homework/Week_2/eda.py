c6671f11a181b23cf7d838a945194f5d7394f7bf
Maud van Boven <maudrvanboven@gmail.com>
2018-11-08 17:09:16 +0100

    comments

--- Homework/Week_2/eda.py
+++ Homework/Week_2/eda.py
@@ -10,33 +10,30 @@ import numpy as np
 import pandas as pd
 
 INPUT_CSV = 'input.csv'
-# OUTPUT_CSV = 'movies.csv'
 
 
 def cleanup(dict_reader):
     """
-    Deletes rows lacking information and removes extra spaces from data.
+    Deletes rows lacking information and removes surplus spaces from data.
     Takes a DictReader and its columns as input. Returns a list of dictionaries.
     """
 
-    print(dict_reader)
-
-    # code source: https://stackoverflow.com/questions/40320170/access-column-using-dictreader
+    # extract column names
     columns = dict_reader.fieldnames
 
-    for column in columns:
-        print(column)
-
+    # save all complete, and 'de-spaced' dictionaries in new list
     clean_rows = []
     for dict in dict_reader:
-        # print(dict)
         row = {}
         info_complete = True
+
+        # remove surplus spaces from data entries and check if they contain info
         for column in columns:
-            # print(dict[column])
             row[column] = dict[column].strip()
             if not row[column] or row[column] == "unknown":
                 info_complete = False
+
+        # save rows with complete information
         if info_complete:
             clean_rows.append(row)
 
@@ -47,8 +44,10 @@ if __name__ == "__main__":
 
     with open(INPUT_CSV, 'r') as infile:
 
+        # extract information from csv input file
         reader = csv.DictReader(infile)
 
+        # preprocess data to only contain full info rows, without surplus spaces
         rows = cleanup(reader)
 
         for row in rows:

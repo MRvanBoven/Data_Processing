ad102991fb5997acc3baa514e7686a3ff2bf76c7
Maud van Boven <maudrvanboven@gmail.com>
2018-11-21 20:19:31 +0100

    minor changes

--- Homework/Week_3/convertCSV2JSON.py
+++ Homework/Week_3/convertCSV2JSON.py
@@ -7,6 +7,7 @@ This script loads in a CSV file and converts it to a JSON output file.
 
 import csv
 import json
+import numpy as np
 import pandas as pd
 
 # source input: https://perso.telecom-paristech.fr/eagan/class/igr204/datasets
@@ -19,8 +20,15 @@ def preprocess(df):
     Preprocesses given data frame. Returns preprocessed data frame.
     """
 
-    # remove first non-title row, which describes the data types in the columns
-    df = df.drop(["STRING"])
+    # remove first non-title row, which describes data types in the columns
+    df = df.drop([0])
+
+    # convert numeric column data to numbers
+    for col in df.columns:
+        df[col] = pd.to_numeric(df[col], errors='ignore')
+
+    # group data by years, calculating each year's average of other values
+    df = df.groupby("Year").mean()
 
     return df
 
@@ -30,7 +38,7 @@ if __name__ == "__main__":
     with open(INPUT_CSV, 'r') as infile:
 
         # create pandas data frame from csv data in infile
-        df = pd.read_csv(infile, sep=';', index_col="Title")
+        df = pd.read_csv(infile, sep=';')
 
         infile.close()
 
@@ -39,5 +47,5 @@ if __name__ == "__main__":
 
     # write columns of interest to json file, replacing NaN with None
     with open(OUTPUT_JSON, 'w') as outfile:
-        json.dump(df[["Year", "Length"]].where((pd.notnull(df)), None).
-                  to_dict(orient='index'), outfile, indent=4) #index_col="Title"
+        json.dump(df.where((pd.notnull(df)), None).to_dict(orient='index'),
+                  outfile, indent=4)

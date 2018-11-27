3a0172798c1031ac98eb070866fc20eb79444539
Maud van Boven <maudrvanboven@gmail.com>
2018-11-27 16:58:02 +0100

    interactivity and labels

--- Homework/Week_4/script.js
+++ Homework/Week_4/script.js
@@ -1,13 +1,16 @@
 // define svg width and height as global variables
 var w = 700;
-var h = 500;
+var h = 600;
 var barPadding = 3;
+var y0Padding = 10;
+var topHTML = 273;
 var margins = {top: 10, bottom: 50, left: 70, right: 0},
     width = w - margins.left - margins.right,
     height = h - margins.top - margins.bottom;
 
 // add title, student data and assignment description to HTML
-d3.select("title")
+d3.select("head")
+  .append("title")
   .text("Time Travel Doctor Who");
 d3.select("body")
   .append("h2")
@@ -20,7 +23,13 @@ d3.select("body")
 d3.select("body")
   .append("p")
   .attr("class", "text")
-  .text("Through how many years of time did each incarnation of the Doctor actually travel? Data source");
+  .text("Through how many years of time did each incarnation of the Doctor " +
+        "actually travel? Name and co created a whole data set concerning " +
+        "this question, as they/he explained in the Guardian/here (= link). " +
+        "In the bar chart below, the cumulative total of all years travelled " +
+        "through by each incarnation of the Doctor is shown. Bear in mind " +
+        "that the y axis has a logarithmic scale! You can hover over the " +
+        "chart to find out more about each Doctor's time travels.");
 
 // add SVG to DOM and remember its reference
 var svg = d3.select("body")
@@ -34,21 +43,30 @@ d3.json(inFile, function(data) {
     var doctors = Object.keys(data).map(x => x);
     var travels = Object.values(data).map(x => parseInt(x.travelled));
 
+    // define bar width for bar chart
     var barWidth = width / travels.length - barPadding;
 
+    // define scales for bar chart
     var scales = scale(doctors, travels, barWidth);
-    var xScale = scales[0];
-    var yScale = scales[1];
+    var xScale = scales[0],
+        yScale = scales[1],
+        y0Scale = scales[2];
 
-    bars(travels, yScale, barWidth);
+    // define tooltip division
+    var tip = d3.select("body")
+                .append("div")
+                .attr("class", "tooltip")
+                .style("opacity", 0);
 
-    labels(travels, yScale, barWidth);
+    // labels(travels, yScale, barWidth);
 
-    axes(xScale, yScale);
+    bars(travels, yScale, barWidth, tip); //tip
+
+    axes(xScale, yScale, y0Scale);
 });
 
 
-function axes(xScale, yScale) {
+function axes(xScale, yScale, y0Scale) {
     // define x axis
     var xAxis = d3.svg.axis()
                       .scale(xScale)
@@ -59,11 +77,16 @@ function axes(xScale, yScale) {
                       .scale(yScale)
                       .orient("left");
 
+    // define y 0 axis
+    var y0Axis = d3.svg.axis()
+                       .scale(y0Scale)
+                       .orient("left");
+
     // create x axis
     svg.append("g")
        .attr("class", "x axis")
        .attr("transform", "translate(" + margins.left + ","
-                                     + (height + margins.top + barPadding)
+                                     + (height + margins.top + y0Padding + barPadding)
                                      + ")")
        .call(xAxis);
 
@@ -74,13 +97,20 @@ function axes(xScale, yScale) {
                                      + "," + margins.top + ")")
        .call(yAxis);
 
-   // add label x axis
-   svg.append("text")
-      .attr("class", "label")
-      .attr("x", margins.left + width / 2)
-      .attr("y", h - margins.bottom / 10)
-      .style("text-anchor", "middle")
-      .text("Incarnation of the Doctor")
+    // create y 0 axis
+    svg.append("g")
+       .attr("class", "axis")
+       .attr("transform", "translate(" + (margins.left - barPadding)
+                                     + "," + (margins.top + height) + ")")
+       .call(y0Axis);
+
+    // add label x axis
+    svg.append("text")
+       .attr("class", "label")
+       .attr("x", margins.left + width / 2)
+       .attr("y", h)
+       .style("text-anchor", "middle")
+       .text("Incarnation of the Doctor")
 
     // add label y axis
     svg.append("text")
@@ -93,7 +123,7 @@ function axes(xScale, yScale) {
 }
 
 
-function bars(yData, yScale, barWidth) {
+function bars(yData, yScale, barWidth, tip) { //tip
     // create bars for bar chart
     var bars = svg.selectAll("rect")
                   .data(yData)
@@ -108,50 +138,62 @@ function bars(yData, yScale, barWidth) {
              if (d !== 0) {
                  return yScale(d);
              }
-             return height;
+             return height + margins.top + y0Padding - 1;
          })
         .attr("width", barWidth)
         .attr("height", function(d) {
-            if (d !== 0) {
-                return height + margins.top - yScale(d);
-            }
-            return 0;
+             if (d !== 0) {
+                 return height + margins.top - yScale(d) + y0Padding;
+             }
+             return 1;
          })
         .attr("fill", function(d) {
              return "rgb(0, 0," + (Math.log10(d)) * 18 + ")";
+         })
+        .on("mouseover", function(d, i) {
+             var x = margins.left + i * width / yData.length;
+             var y = 0;
+             if (d !== 0) {
+                 y = topHTML + yScale(d) - barPadding;
+             }
+             else {
+                 y = topHTML + height + margins.top + y0Padding - barPadding;
+             }
+            console.log(y);
+
+             d3.select(this)
+               .style("fill", function(d) {
+                    return "rgb(" + (Math.log10(d)) * 15 + ", 0,"
+                               + (Math.log10(d)) * 18 + ")";
+                });
+
+             tip.transition()
+                .duration(200)
+                .style("opacity", .9);
+             tip.html(label(d))
+                .style("left", x + "px")
+                .style("top", y + "px");
+         })
+        .on("mouseout", function(d) {
+             d3.select(this)
+               .style("fill", function(d) {
+                    return "rgb(0, 0," + (Math.log10(d)) * 18 + ")";
+                });
+
+             tip.transition()
+                .duration(500)
+                .style("opacity", 0);
          });
 }
 
 
-function labels(yData, yScale, barWidth) {
-    // add bar labels
-    svg.selectAll("text")
-       .data(yData)
-       .enter()
-       .append("text")
-       .text(function (d) {
-            if (d !== 0) {
-                var log = Math.floor(Math.log10(d));
-                return (d / (Math.pow(10, log))).toFixed(2) + "e" + log;
-            }
-            return d.toFixed(2);
-        })
-       .attr("x", function(d, i) {
-            return margins.left + i * width / yData.length + barWidth / 2;
-        })
-       .attr("y", function(d) {
-             if (d !== 0) {
-                 return yScale(d) - barPadding;
-             }
-             return height + margins.top - barPadding;
-        })
-       .attr("text-anchor", "middle")
-       .attr("font-family", "sans-serif")
-       .attr("font-size", "11px")
-       .attr("fill", function(d) {
-             return "rgb(0, 0," + (Math.log10(d)) * 18 + ")";
-        });
-
+function label(d) {
+    // convert d to scientific power notation with "e"
+    if (d !== 0) {
+        var log = Math.floor(Math.log10(d));
+        return "<span>" + (d / Math.pow(10, log)).toFixed(2) + "e" + log + "</span>"
+    }
+    return "<span>" + d.toFixed(2) + "</span>";
 }
 
 
@@ -168,5 +210,10 @@ function scale(xData, yData, barWidth) {
                          .rangeRound([height, 0])
                          .nice();
 
-    return [xScale, yScale];
+    // define 0 scale for y axis
+    var y0Scale = d3.scale.ordinal()
+                          .domain([0])
+                          .range([y0Padding]);
+
+    return [xScale, yScale, y0Scale];
 }

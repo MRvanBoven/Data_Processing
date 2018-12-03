34f2e11e6c8b3d1f9e3c3921fa29fd1fcfb10030
Maud van Boven <maudrvanboven@gmail.com>
2018-12-03 18:09:00 +0100

    data parsed to scatter plot usable datapoints

--- Homework/Week_5/scatter.js
+++ Homework/Week_5/scatter.js
@@ -18,17 +18,104 @@ function getData() {
     // request to read in JSONs
     let requests = [d3.json(womenInScience), d3.json(consConf)];
 
-    // if JSONS are read in successfully, let them be converted to easier format
+    // if JSONS are read in successfully, got to main function
     Promise.all(requests)
            .then(function(response) {
-                data1 = transformResponse(response[0]);
-                data2 = transformResponse(response[1]);
-                console.log(data1);
-                console.log(data2);
+                main(response);
             })
            .catch(function(e) {
                 throw(e);
             });
+}
+
+
+/**
+ * Main function, started once a successful response has been received.
+ */
+function main(response) {
+    // define svg dimensions
+    let w = 700;
+    let h = 600;
+    let barPadding = 3;
+    let y0Padding = 10;
+    let margins = {top: 30, bottom: 50, left: 70, right: 0},
+        width = w - margins.left - margins.right,
+        height = h - margins.top - margins.bottom;
+
+    // let responses be converted to easier data formats
+    let data1 = transformResponse(response[0]);
+    let data2 = transformResponse(response[1]);
+
+    // convert dataSets to lists of data points per year
+    let dataPoints = transformData(data1, data2);
+    console.log(dataPoints);
+}
+
+
+/**
+ * Transforms given data sets to list of data point per year. Returns the lists.
+ */
+function transformData(data1, data2) {
+    // save data from data1 in object with data point lists for each year
+    let allYears = {};
+    data1.forEach(function(series) {
+        // find location of years and their observation values, remember country
+        let wom = Object.keys(series)[0];
+        let country = Object.keys(series[wom])[0];
+        let years = series[wom][country];
+
+        // find years and save them in object with corresponding data points
+        Object.keys(years).forEach(function(index) {
+            // find year
+            let year = Object.keys(years[index])[0];
+
+            // add year to object, if not already in it
+            if (!(year in allYears)) {
+                allYears[year] = [];
+            }
+
+            // save datapoint with country and wom value in list of right year
+            let value = years[index][year];
+            let dataPoint = [country, value];
+            allYears[year].push(dataPoint);
+        });
+    });
+
+    // add consConf data to right data points in right year lists
+    data2.forEach(function(series) {
+        // find location of years and their observation values, remember country
+        let country = Object.keys(series)[0];
+        let consConf = Object.keys(series[country])[0];
+        let years = series[country][consConf]["Annual"];
+
+        // find years and add consConf value to data point with same year & coun
+        Object.keys(years).forEach(function(index) {
+            // find year
+            let year = Object.keys(years[index])[0];
+            let value = years[index][year];
+
+            // if data point with same year & coun in object add consConf value
+            if (year in allYears) {
+                allYears[year].forEach(function(dataPoint) {
+                    if (dataPoint[0] === country) {
+                        dataPoint.push(value);
+                        return;
+                    }
+                });
+            }
+        });
+    });
+
+    // test if all data points have 3 variables, if not delete them
+    Object.keys(allYears).forEach(function(year) {
+        allYears[year].forEach(function(dataPoint) {
+            if (dataPoint.length !== 3) {
+                allYears[year].splice(allYears[year].indexOf(dataPoint), 1);
+            }
+        })
+    })
+
+    return allYears;
 }
 
 
